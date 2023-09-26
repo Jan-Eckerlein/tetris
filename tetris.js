07d88a456059fe9a51451913e67810c7800200ef
jan-eckerlein <eckerlein.jan@gmail.com>
2023-09-26 07:35:43 +0200

    gameover

--- tetris.js
+++ tetris.js
@@ -1,6 +1,6 @@
 export const createTetrisGame = (htmlNode) => {
 	const width = 10;
-	const height = 5;
+	const height = 3;
 	const tileWidth = 30;
 	const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'cyan'];
 	const interval = 1000;
@@ -38,21 +38,27 @@ export const createTetrisGame = (htmlNode) => {
 
 
 	// Setup game board
-	gameBoardElement.setAttribute('style', `grid-template-columns: repeat(${width}, 1fr); grid-template-rows: repeat(${height}, 1fr)`);
+	const setupBoard = () => {
+		gameBoardElement.setAttribute('style', `grid-template-columns: repeat(${width}, 1fr); grid-template-rows: repeat(${height}, 1fr)`);
+	}
 
 	// Create tiles
-	for (let i = 0; i < width * height; i++) {
-		const tile = document.createElement('div');
-		tile.setAttribute('class', 'tile');
-		tile.setAttribute('id', `tile${i}`);
-		gameBoardElement.appendChild(tile);
+	const createTiles = () => {
+		for (let i = 0; i < width * height; i++) {
+			const tile = document.createElement('div');
+			tile.setAttribute('class', 'tile');
+			tile.setAttribute('id', `tile${i}`);
+			gameBoardElement.appendChild(tile);
+		}
 	}
 
 	// Init game board array
-	for (let row = 0; row < height; row++) {
-		gameBoard[row] = [];
-		for (let col = 0; col < width; col++) {
-			gameBoard[row][col] = false;
+	const initGameBoard = () => {
+		for (let row = 0; row < height; row++) {
+			gameBoard[row] = [];
+			for (let col = 0; col < width; col++) {
+				gameBoard[row][col] = false;
+			}
 		}
 	}
 
@@ -119,9 +125,9 @@ export const createTetrisGame = (htmlNode) => {
 	};
 
 	const clearBoard = () => {
-		const movingTiles = gameBoardElement.querySelectorAll('[data-moving-tile]');
+		const movingTiles = gameBoardElement.querySelectorAll('[data-moving-tile], [data-active-tile]');
 		movingTiles.forEach(tile => {
-			delete tile.dataset.movingTile;
+			delete tile.dataset.movingTile ;
 			tile.style.removeProperty('background-color');
 		});
 	}
@@ -131,7 +137,7 @@ export const createTetrisGame = (htmlNode) => {
 			row.forEach((value, x) => {
 				const tile = document.getElementById(`tile${y * width + x}`);
 				if (value) {
-					tile.dataset.activeTile;
+					tile.dataset.activeTile = true;
 					tile.style.backgroundColor = value;
 				} else {
 					delete tile.dataset.activeTile;
@@ -142,11 +148,20 @@ export const createTetrisGame = (htmlNode) => {
 
 	const gameLoop = (shape, shapeId) => {
 		clearBoard();
+		console.log({gameBoard});
 		if (!shape) {
 			shape = createNewShape(shapeId++);
+			if (!shape.checkCollision()) {
+				alert('Game over!');
+				shape = null;
+				gameBoard = [];
+				initGameBoard();
+				return null;
+			}
+			shape.draw();
 		}
 
-		if (shape.moveDown()) {
+		else if (shape.moveDown()) {
 			shape.draw();
 		} else {
 			shape.addToGameBoard(gameBoard);
@@ -160,6 +175,10 @@ export const createTetrisGame = (htmlNode) => {
 	const startGame = () => {
 		let shape;
 		let shapeId = 0;
+
+		setupBoard();
+		createTiles();
+		initGameBoard();
 
 		setInterval(() => {
 			shape = gameLoop(shape, shapeId);
